26971d8858ab37893790722a3e465c068157543a
Pete <fangorn.treebeard.1962@gmail.com>
2015-09-29 14:14:01 -0500

    fixing unit test to validate travis setup

--- server/api/thing/thing.spec.js
+++ server/api/thing/thing.spec.js
@@ -18,8 +18,4 @@ describe('GET /api/things', function() {
       });
   });
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
-
 });
