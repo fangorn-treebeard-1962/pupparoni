8562eb37dd4327819a2b2b42c062ae04da4454c0
Pete <fangorn.treebeard.1962@gmail.com>
2015-10-14 15:19:00 -0500

    adding initial product get testing

--- client/app/product/product.controller.js
+++ client/app/product/product.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('pupparoniApp')
-  .controller('ProductCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('ProductCtrl', function ($scope, $http, socket) {
+    $scope.product = '';
+    
+    $http.get('/api/product/:id').success(function(product) {
+      $scope.product = product;
+      socket.syncUpdates('thing', $scope.awesomeThings);
+    });
+
   });
--- client/app/product/product.controller.spec.js
+++ client/app/product/product.controller.spec.js
@@ -4,18 +4,35 @@ describe('Controller: ProductCtrl', function () {
 
   // load the controller's module
   beforeEach(module('pupparoniApp'));
+  beforeEach(module('socketMock'));
 
-  var ProductCtrl, scope;
+  var ProductCtrl,
+      scope,
+      $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expect('GET', /\/api\/product\/(.+)/, undefined, undefined, ['id'])
+      .respond({
+        name:"Siamese If You Please",
+        productId:"0001F",
+        category:"Behavior",
+        shortDescription:"Lavender based topical anti-stress therapy",
+        longDescription:"Apply one dose weekly to your pet’s haircoat to reduce overall stress levels for your cat",
+        tags:["lavender", "anti-stress", "feline"],
+        size:"20 ml bottle",
+        price:"21.99"});
     scope = $rootScope.$new();
     ProductCtrl = $controller('ProductCtrl', {
       $scope: scope
     });
   }));
 
-  it('should ...', function () {
-    expect(1).toEqual(1);
+  it('should attach a product to the scope', function () {
+    $httpBackend.flush();
+    expect(scope.product).toBeDefined();
+    console.log(scope.product.name);
+    expect(scope.product.name).toEqual('Siamese If You Please');
   });
 });
