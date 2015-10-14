86854fe426ce4589c625bd759f737c3f5be4a750
Pete <fangorn.treebeard.1962@gmail.com>
2015-10-14 13:47:16 -0500

    wiring up test mock

--- client/app/productList/productList.controller.spec.js
+++ client/app/productList/productList.controller.spec.js
@@ -4,20 +4,27 @@ describe('Controller: ProductListCtrl', function () {
 
   // load the controller's module
   beforeEach(module('pupparoniApp'));
+  beforeEach(module('socketMock'));
 
-  var ProductListCtrl, scope;
+  var ProductListCtrl, 
+      scope,
+      $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('/api/products')
+      .respond(['product1', 'product2', 'product3', 'product4']);
+
     scope = $rootScope.$new();
     ProductListCtrl = $controller('ProductListCtrl', {
       $scope: scope
     });
   }));
 
-  it('should create "productList" model with 13 products', function () {
-
-    expect(scope.productList.length).toBe(13);
+  it('should create "productList" model with 4 products', function () {
+    $httpBackend.flush();
+   expect(scope.productList.length).toBe(4);
   });
 
 });
