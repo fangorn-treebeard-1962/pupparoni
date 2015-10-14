26cfa02c4912ec70d7f441616efdff65b645bf8d
Pete <fangorn.treebeard.1962@gmail.com>
2015-10-14 10:14:26 -0500

    initial check-in testing only navbar

---
+++ e2e/company/support/support.po.js
@@ -0,0 +1,20 @@
+/**
+ * This file uses the Page Object pattern to define the main page for tests
+ * https://docs.google.com/presentation/d/1B6manhG0zEXkC-H-tPo2vwU06JhL8w9-XCF9oehXzAQ
+ */
+
+'use strict';
+
+var CompanySupportPage = function() {
+
+  this.navBar = element(by.css('div.navbar'));
+  this.homeLink = element(by.cssContainingText('a', 'Home'));
+  this.loginLink = element(by.cssContainingText('a', 'Login'));
+  this.registerLink = element(by.cssContainingText('a', 'Sign up'));
+  this.companyLink = element(by.cssContainingText('a', 'Company'));
+  this.productsLink = element(by.cssContainingText('a', 'Products'));
+
+};
+
+module.exports = new CompanySupportPage();
+
---
+++ e2e/company/support/support.spec.js
@@ -0,0 +1,41 @@
+'use strict';
+
+describe('Company Support View', function() {
+  var page;
+
+  beforeEach(function() {
+    browser.get('/company/support');
+    page = require('./support.po.js');
+  });
+
+  it('should have navbar', function() {
+    expect(page.navBar).toBeDefined();
+  });
+
+  it('should have Company active in navbar', function() {
+    expect(page.getActiveNavbarLink().getText()).toBe("Company");
+  });
+
+  it('should include expected navigation links', function () {
+    expect(page.loginLink.getText()).toBe('Login');
+    expect(page.registerLink.getText()).toBe('Sign up');
+    expect(page.companyLink.getText()).toBe('Company');
+    expect(page.productsLink.getText()).toBe('Products');
+  });
+
+  it('should navigate to the Home up page when the Mome link is clicked', function () {
+    page.homeLink.click();
+    expect(browser.getCurrentUrl()).toContain('/');
+  });
+
+  it('should navigate to the Sign up page when the register link is clicked', function () {
+    page.registerLink.click();
+    expect(browser.getCurrentUrl()).toContain('/signup');
+  });
+
+  it('should navigate to the Login page when the Login link is clicked', function () {
+    page.loginLink.click();
+    expect(browser.getCurrentUrl()).toContain('/login');
+  });
+
+});
