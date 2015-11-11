a52865ac3d2e3d3dcf7f67ba1efbb626de668de4
Pete <fangorn.treebeard.1962@gmail.com>
2015-11-11 07:17:31 -0600

    adding searchByName test

--- server/api/product/product.spec.js
+++ server/api/product/product.spec.js
@@ -8,13 +8,30 @@ describe('GET /api/products', function() {
 
   it('should respond with JSON array', function(done) {
     request(app)
-      .get('/api/products')
+      .get('/api/products/')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
         if (err) return done(err);
         res.body.should.be.instanceof(Array);
-        console.dir(res.body);
+        done();
+      });
+  });
+});
+
+describe('GET /api/products/name/Pugly', function() {
+
+  it('should respond with JSON array', function(done) {
+    request(app)
+      .get('/api/products/name/Pugly')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Array);
+        res.body.length.should.be.equal(1);
+
+        res.body[0].name.should.be('Pugly');
         done();
       });
   });
