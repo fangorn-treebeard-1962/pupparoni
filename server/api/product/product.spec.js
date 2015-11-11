1a8e28d14da400bc892c2a7fd95387e1a5bffe88
Pete <fangorn.treebeard.1962@gmail.com>
2015-11-11 08:31:57 -0600

    changing tests to simply confirm api returns expected type of object

--- server/api/product/product.spec.js
+++ server/api/product/product.spec.js
@@ -19,19 +19,64 @@ describe('GET /api/products', function() {
   });
 });
 
-describe('GET /api/products/name/Pugly', function() {
+describe('GET /api/products/<field>/<value>', function() {
 
-  it('should respond with JSON array', function(done) {
+  it('should respond with JSON array for name lookup', function(done) {
     request(app)
-      .get('/api/products/name/Pugly')
+      .get('/api/products/name/name')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
         if (err) return done(err);
         res.body.should.be.instanceof(Array);
-        res.body.length.should.be.equal(1);
+        done();
+      });
+  });
 
-        res.body[0].name.should.be('Pugly');
+  it('should respond with JSON array for category lookup', function(done) {
+    request(app)
+      .get('/api/products/category/category')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Array);
+        done();
+      });
+  });
+
+  it('should respond with JSON array for tag lookup', function(done) {
+    request(app)
+      .get('/api/products/tag/tag')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Array);
+        done();
+      });
+  });
+
+  it('should respond with JSON array for description lookup', function(done) {
+    request(app)
+      .get('/api/products/description/description')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Array);
+        done();
+      });
+  });
+
+  it('should respond with JSON object for productId lookup', function(done) {
+    request(app)
+      .get('/api/products/productId/productId')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Object);
         done();
       });
   });
